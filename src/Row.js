168b836dc81e9bb97dae3fcbf940e162dcf6c103
Noble Menchaca <noble.menchaca@gmail.com>
2022-04-15 18:26:30 -0500

    Fixed movie trailers not loading on click

--- src/Row.js
+++ src/Row.js
@@ -6,8 +6,6 @@ import movieTrailer from "movie-trailer"
 
 const imgURL = 'https://image.tmdb.org/t/p/original/';
 
-
-
 function Row({ title, fetchURL, isLargeRow }) {
     const [movies, setMovies] = useState([]);
     const [trailerURL, settrailerURL] = useState("")
@@ -34,37 +32,37 @@ function Row({ title, fetchURL, isLargeRow }) {
         if (trailerURL) {
             settrailerURL('');
         } else {
-            movieTrailer((movie?.name || ""), (error, response) => {
-                console.log(error, response);
-            })
-            .then((url) => {
+            movieTrailer(
+              movie?.name || movie?.title || movie?.original_name || ""
+            )
+              .then((url) => {
                 const urlParams = new URLSearchParams(new URL(url).search);
-                console.log(urlParams.get('v'));
-                settrailerURL(urlParams.get('v'));
-            })
-            .catch((error) => console.log(error))
+                settrailerURL(urlParams.get("v"));
+              })
+              .catch((error) => console.log(error));
         }
     }
 
     return (
-    <div className='row'>
+      <div className="row">
         <h2>{title}</h2>
 
-        <div className='row_posters'>
-            {movies.map(movie => (
-                <img 
-                key={movie.id}
-                onClick={() => handleClick(movie)}
-                className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-                src={`${imgURL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-                alt={movie.name} />
-            ))}
+        <div className="row_posters">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              onClick={() => handleClick(movie)}
+              className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+              src={`${imgURL}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          ))}
         </div>
-        {/* constiainer -> posters */}
         {trailerURL && <YouTube videoId={trailerURL} opts={opts} />}
-
-    </div>
-  )
+      </div>
+    );
 }
 
 export default Row
